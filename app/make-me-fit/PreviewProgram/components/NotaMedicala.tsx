c24d67107d5151caeb0e05bf1118645c659abaae
SergiuCiausu <ciausu.sergiu@gmail.com>
2025-06-01 11:49:05 +0300

    Update NotaMedicala.tsx

--- app/make-me-fit/PreviewProgram/components/NotaMedicala.tsx
+++ app/make-me-fit/PreviewProgram/components/NotaMedicala.tsx
@@ -9,7 +9,7 @@ const NotaMedicala = () => {
             marginBottom: "var(--nota-medicala-mb)",
             fontSize: "var(--p-size)"
         }}>
-        "Poate fi urmat de orice femeie fără probleme de sănătate cardiace sau cronice"
+        Poate fi urmat de orice femeie fără probleme de sănătate cardiace sau cronice
     </p>
   )
 }
