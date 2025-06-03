e9dcdd991133246fa0938c5f6496ad7616bac7bc
SergiuCiausu <ciausu.sergiu@gmail.com>
2025-06-03 17:19:34 +0300

    Update Produse.tsx

--- app/case-smart/produse/components/Produse.tsx
+++ app/case-smart/produse/components/Produse.tsx
@@ -1,35 +1,13 @@
 import ProductCard from '@/app/case-smart/components/ProductCard'
 import React from 'react'
-
-interface Variant {
-    culoare: string,
-    img: string
-}
-
-interface Produs {
-    id: number
-    denumire_produs: string,
-    rating: number,
-    img: string,
-    reducere: number,
-    totalRecenzii: number
-    brand: string,
-    categorie: string,
-    pret: number,
-    variants: Variant[],
-    url: string
-}
-
-interface ProduseProps {
-    produse: Produs[];
-}
+import type { ProduseProps } from "@/app/case-smart/produse/types"
 
 const Produse = ({ produse, isSingleFiltruCuloare } : { produse: ProduseProps, isSingleFiltruCuloare: boolean}) => {
   return (
     <div
         className="grid grid-cols-3 gap-5">
-        {produse.produse.length > 0
-        ? produse.produse.map((produs) => (
+        {produse!.produse.length > 0
+        ? produse!.produse.map((produs) => (
                 <ProductCard 
                     key={produs.id}
                     title={produs.denumire_produs}
