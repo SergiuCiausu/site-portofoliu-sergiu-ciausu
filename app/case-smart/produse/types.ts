a2ee14fc539cb37a377817d692596a77dbaeb56c
SergiuCiausu <ciausu.sergiu@gmail.com>
2025-06-03 17:19:00 +0300

    Update types.ts

--- app/case-smart/produse/types.ts
+++ app/case-smart/produse/types.ts
@@ -3,3 +3,32 @@ export interface FilterValueObject {
 }
 
 export type FilterValue = string | number | FilterValueObject | (string | number)[];
+
+export interface FilterValueObject {
+  [key: string]: FilterValue;
+}
+
+export type FilterValue = string | number | FilterValueObject | (string | number)[];
+
+interface Variant {
+    culoare: string,
+    img: string
+}
+
+interface Produs {
+    id: number
+    denumire_produs: string,
+    rating: number,
+    img: string,
+    reducere: number,
+    totalRecenzii: number
+    brand: string,
+    categorie: string,
+    pret: number,
+    variants: Variant[],
+    url: string
+}
+
+export type ProduseProps = {
+    produse: Produs[];
+}
