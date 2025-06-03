1ba283c12c56fc1223a47d0f98333ad7f8ee70cf
SergiuCiausu <ciausu.sergiu@gmail.com>
2025-06-03 18:57:55 +0300

    Update client.ts

--- prisma/client.ts
+++ prisma/client.ts
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: ['error', 'warn'],
   });
 
 export default prisma;
